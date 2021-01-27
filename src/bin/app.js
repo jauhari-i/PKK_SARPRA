6162a08c861061b6fcabe2f64ed8e3e962ffe588
Irfan Jauhari <kangrykong123@gmail.com>
2021-01-27 13:20:02 +0700

    Update build

--- src/bin/app.js
+++ src/bin/app.js
@@ -1,20 +1,14 @@
 import express from 'express'
 import configureApp from '../config/configureApp'
 import { getConfig } from '../config/global_config'
-import { db as mysqlConnection } from '../db/mysqlConnection'
+import connectMongo from '../db/mongoConnection'
+import mongoose from 'mongoose'
 
 const port = getConfig('/port')
 const app = express()
 
-mysqlConnection.getConnection((err, connection) => {
-  if (connection) {
-    console.log('Connected to mysql database')
-  } else {
-    console.log(err)
-  }
-})
-
 configureApp(app)
+connectMongo(mongoose)
 
 app.listen(port, () => {
   console.log('Server is running on port ' + port)
