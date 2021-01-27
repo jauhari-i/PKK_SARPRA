fae15c1fda1bb3cb8915905e64a9e0779bdbd62c
Irfan Jauhari <kangrykong123@gmail.com>
2021-01-27 13:04:51 +0700

    Mysql Setting

--- src/db/mysqlConnection.js
+++ src/db/mysqlConnection.js
@@ -1,10 +1,12 @@
 import { getConfig } from '../config/global_config'
-import { createPool } from 'mysql'
+import mysql from 'mysql'
 const mysqlConfig = getConfig('/mysqlConfig')
 
-const db = createPool({
+const db = mysql.createPool({
   connectionLimit: mysqlConfig.connectionLimit,
+  acquireTimeout: 30000,
   host: mysqlConfig.host,
+  port: mysqlConfig.port,
   user: mysqlConfig.user,
   password: mysqlConfig.password,
   database: mysqlConfig.database,
